a5dbaf59ae7990b8846780a59eedab927244b77f
user <savaliyaparth712@gmail.com>
2022-12-27 11:46:22 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:stackproject/main.dart';
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(
+      MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: Stack(),
+         ),
+       );
+}
